fab75382be882f3204a48f0faec6911fab1e17a9
Matt Davis <matthewpeterdavis@gmail.com>
2018-01-15 18:24:17 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ require("font-awesome/css/font-awesome.css");
 // Require index.html so it gets copied to dist
 require("./index.html");
 
-var Elm = require("./Main.Elm");
+var Elm = require("./Main.elm");
 var mountNode = document.getElementById("main");
 
 // .embed() can take an optional second argument. This would be an object describing the data we need to start a program, i.e. a userID or some token
