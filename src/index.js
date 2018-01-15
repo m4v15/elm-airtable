79543f13198d4a7200eed595110d3464f3be25ed
Matt Davis <matthewpeterdavis@gmail.com>
2018-01-15 17:45:02 +0000

    change file name

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-'use strict'
+"use strict";
 
-require('ace-css/css/ace.css')
-require('font-awesome/css/font-awesome.css')
+require("ace-css/css/ace.css");
+require("font-awesome/css/font-awesome.css");
 
 // Require index.html so it gets copied to dist
-require('./index.html')
+require("./index.html");
 
-var Elm = require('./Main.elm')
-var mountNode = document.getElementById('main')
+var Elm = require("./Main.Elm");
+var mountNode = document.getElementById("main");
 
 // .embed() can take an optional second argument. This would be an object describing the data we need to start a program, i.e. a userID or some token
-var app = Elm.Main.embed(mountNode)
+var app = Elm.Main.embed(mountNode);
